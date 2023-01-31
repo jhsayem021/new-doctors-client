1998435909ba6f3736323e0378cd2dff28a0cdb3
jhsayem02101~ <sayemmetro420@gmail.com>
2023-01-31 18:07:02 +0600

    About and Contact page make

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import About from "../../Pages/About/About";
 import Appointment from "../../Pages/Appointment/Appointment/Appointment";
+import Contact from "../../Pages/Contact/Contact";
 import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
@@ -24,6 +26,16 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            }
+            ,
+            {
+                path: '/about',
+                element: <About></About>
+            }
+            ,
+            {
+                path: '/contact',
+                element: <Contact></Contact>
             }
             ,
             {
